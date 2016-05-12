ace9f2eb3e6adcd67f4b4b8556358539d7ba22d1
ngoyet-ndiaye <ngoyet-ndiaye@windows-test6>
2016-05-12 10:27:08 +0200

    add Name cheikh

--- src/main/java/Test/Connexion.java
+++ src/main/java/Test/Connexion.java
@@ -142,6 +142,7 @@ public class Connexion extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Connexion().setVisible(true);
+                connexion_en_cour c = new connexion_en_cour
             }
         });
     }
